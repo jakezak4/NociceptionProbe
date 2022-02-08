f2d4f46055b001b28f015923a09d3c739b5fa93a
Jacob Jaszczak <jacob.jaszczak@gmail.com>
2022-02-08 12:12:33 -0800

    made thermocouple function

--- NociceptionProbe.ino
+++ NociceptionProbe.ino
@@ -70,7 +70,7 @@ char peltierPower[4];
 
 void setup(){
   delay(250);                            // give chip a chance to stabilize
-  Serial.begin(38400);                   // set baudrate of serial port
+  Serial.begin(115200);                   // set baudrate of serial port
   //Serial.println("Playing With Fusion: MAX31856, SEN-30005;");
 
   // setup for the the SPI library:
@@ -96,6 +96,9 @@ void setup(){
 
   digitalWrite(URC10_MOTOR_1_DIR, 1); //Board motor controler current direction  
 
+  Serial.print("#Target temp is ");
+  Serial.println(targetTemp);
+  
   //Serial.println("Int-Temp,Ext-Temp,Target%,Offset"); 
   Serial.println("Int-Temp,Ext-Temp,Target%,Offset,PWM"); //Trouble shooting
 }
@@ -119,87 +122,17 @@ void loop(){
 // ##### Thermocouple code ##############################  
   delay(500);                                   // 500ms delay... can be as fast as ~100ms in continuous mode, 1 samp avg
   
-  static struct var_max31856 TC_CH0, TC_CH1;
   double tmp0; // thermocouple #1
   double tmp1; // thermocouple #2
    
-  struct var_max31856 *tc_ptr;
+  read_thermocouple(tmp0, tmp1);
+  Serial.print(tmp0); // print temperature sensor 0 
+  Serial.print(",");
+  Serial.print(tmp1); // print temperature sensor 1
+  Serial.print(",");
   
-  // Read CH 0
-  tc_ptr = &TC_CH0;                             // set pointer
-  thermocouple0.MAX31856_update(tc_ptr);        // Update MAX31856 channel 0
-  // Read CH 1
-  tc_ptr = &TC_CH1;                             // set pointer
-  thermocouple1.MAX31856_update(tc_ptr);        // Update MAX31856 channel 1
-  
-  
-// ##### Print thermo information to serial port ##############################
-
-  // Thermocouple channel 0
-  //Serial.print("Int-Tmp ");            // Print TC0 header
-  if(TC_CH0.status)
-  {
-    // lots of faults possible at once, technically... handle all 8 of them
-    // Faults detected can be masked, please refer to library file to enable faults you want represented
-    Serial.println("fault(s) detected");
-    Serial.print("Fault List: ");
-    if(0x01 & TC_CH0.status){Serial.print("OPEN  ");}
-    if(0x02 & TC_CH0.status){Serial.print("Overvolt/Undervolt  ");}
-    if(0x04 & TC_CH0.status){Serial.print("TC Low  ");}
-    if(0x08 & TC_CH0.status){Serial.print("TC High  ");}
-    if(0x10 & TC_CH0.status){Serial.print("CJ Low  ");}
-    if(0x20 & TC_CH0.status){Serial.print("CJ High  ");}
-    if(0x40 & TC_CH0.status){Serial.print("TC Range  ");}
-    if(0x80 & TC_CH0.status){Serial.print("CJ Range  ");}
-    Serial.println(" ");
-  }
-  else  // no fault, print temperature data
-  {
-    //Serial.println("no faults detected");
-    // MAX31856 Internal Temp
-    tmp0 = (double)TC_CH0.ref_jcn_temp * 0.015625;  // convert fixed pt # to double
-    //Serial.print("Tint = ");                      // print internal temp heading
-    if((-100 > tmp0) || (150 < tmp0)){Serial.println("unknown fault");}
-    else{Serial.print("");}
-    
-    // MAX31856 External (thermocouple) Temp
-    tmp0 = (double)TC_CH0.lin_tc_temp * 0.0078125;           // convert fixed pt # to double
-    Serial.print(tmp0); // print temperature sensor 0 
-    Serial.print(",");
-  }
-
-  // Thermocouple channel 1
-  //Serial.print("Ext-Tmp ");            // Print TC0 header
-  if(TC_CH1.status)
-  {
-    // lots of faults possible at once, technically... handle all 8 of them
-    // Faults detected can be masked, please refer to library file to enable faults you want represented
-    Serial.println("fault(s) detected");
-    Serial.print("Fault List: ");
-    if(0x01 & TC_CH1.status){Serial.print("OPEN  ");}
-    if(0x02 & TC_CH1.status){Serial.print("Overvolt/Undervolt  ");}
-    if(0x04 & TC_CH1.status){Serial.print("TC Low  ");}
-    if(0x08 & TC_CH1.status){Serial.print("TC High  ");}
-    if(0x10 & TC_CH1.status){Serial.print("CJ Low  ");}
-    if(0x20 & TC_CH1.status){Serial.print("CJ High  ");}
-    if(0x40 & TC_CH1.status){Serial.print("TC Range  ");}
-    if(0x80 & TC_CH1.status){Serial.print("CJ Range  ");}
-    Serial.println(" ");
-  }
-  else  // no fault, print temperature data
-  {
-    //Serial.println("no faults detected");
-    // MAX31856 Internal Temp
-    tmp1 = (double)TC_CH1.ref_jcn_temp * 0.015625;  // convert fixed pt # to double
-    
-    // MAX31856 External (thermocouple) Temp
-    tmp1 = (double)TC_CH1.lin_tc_temp * 0.0078125;           // convert fixed pt # to double
-    Serial.print(tmp1); // print temperature sensor 1
-    Serial.print(",");
-  }
-
   float currentTime = (millis() - startTime)/1000.0;
-  
+
 // ################ PWM control ########################################
 
   //PID 1.2.0
