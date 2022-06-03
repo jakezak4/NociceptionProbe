963b312dd036f8f03136e0245e0fddff50c88d1a
Jacob Jaszczak <jacob.jaszczak@gmail.com>
2022-06-03 13:36:43 -0700

    Time fix
    Made delay programable. Made read interval consistent. New bug in SPAL of not responding after >5min run

--- NociceptionProbe.ino
+++ NociceptionProbe.ino
@@ -1,4 +1,4 @@
-//This will PWM magnet wire with two k-type thermalcouples for use as a nociceptive probe
+//PWM magnet wire with two k-type thermalcouples for use as a nociceptive probe
 
 /***************************************************************************
 This will heat magnet wire by PWM with two k-type thermalcouples for use as a nociceptive probe
@@ -14,21 +14,28 @@ Thermocouple code from PlayingSEN30006_MAX31856_example.ino
 
 // ##### Assay variables ###############################################
 // #####################################################################
-int Probe_targetTemp = 38; //target temperature of inside probe 
-int Plate_targetTemp = 25; //temperature of assay plate
-float assayTime = 60*240; // 4hrs in seconds 
+float Probe_targetTemp = 46.0; //target temperature of inside probe 
+float Plate_targetTemp = 25.0; //temperature of assay plate
+unsigned long assayTime = 4UL*3600000; // 4hrs in milliseconds 
 // #####################################################################
 // #####################################################################
 
-float probeOffset = 0; //changes target temp by 0.5oC
-float plateOffset = 0; //changes target temp by 0.5oC
+float probeOffset = 0.0; //changes target temp by 0.5oC
+float plateOffset = 0.0; //changes target temp by 0.5oC
 float caliProbe_targetTemp = Probe_targetTemp + probeOffset;
 float caliPlate_targetTemp = Plate_targetTemp + plateOffset;
+float diffProbeTarget = 0.0;
+float diffPlateTarget = 0.0;
+
+
 int PWMmax = 200; //constrain scaling to not burn magnet wire 
 int holdingPWM = 0; // Store PWM to hold power for calibration 
 bool calibration = false; 
 bool enterPWMhold = false;
 bool endHeat = false;
+
+bool buttonPause = false; //debounce for offset buttons
+unsigned long buttonTime = 0L; 
 
 //Playing With Fusion breakout 
 
@@ -39,7 +46,10 @@ PWF_MAX31856  thermocouple0(TC0_CS);
 PWF_MAX31856  thermocouple1(TC1_CS);
 
 //Program variables 
-float startTime; //store time at which the program is started after button click 
+unsigned long startTime; //store time at which the program is started after button click  
+float loopCount = 0.0;
+unsigned long runInterval = 125; // milliseconds interval of read-PID-print loop 
+unsigned long loopMillis; //store last time of loop
 int limitWireOutput; // contrained to 0-255 
 int limitPeltierOutput;
 
@@ -50,11 +60,10 @@ double Setpoint_plate, Input_plate, Output_plate, gap_plate;
 
 //Define the aggressive and conservative Tuning Parameters
 //int gapSet_probe = 0; //conservative PID control is off 
-double aggKp_probe=36, aggKi_probe=30, aggKd_probe=5;
-double consKp_probe=36, consKi_probe=30, consKd_probe=5;
-//double consKp_probe=24, consKi_probe=2, consKd_probe=1;
+//double aggKp_probe=36, aggKi_probe=30, aggKd_probe=5;
+//double consKp_probe=36, consKi_probe=30, consKd_probe=5;
 
-double Kp_probe=consKp_probe,Ki_probe=consKi_probe,Kd_probe=consKd_probe;
+double Kp_probe=36,Ki_probe=20,Kd_probe=5;
 double Kp_plate=100,Ki_plate=20,Kd_plate=0;
 
 //Specify the links and initial tuning parameters
@@ -66,8 +75,6 @@ PID myPID_plate(&Input_plate, &Output_plate, &Setpoint_plate, Kp_plate, Ki_plate
 //Button controler settings 
 int sysOnLED = 8; // LED system start
 
-//int rangeGoLED = 18; // Range good to use
-//int rangeStopLED = 19; // Outside range
 int targetLED = A4; // target gap LED 
 int PWMLED = A5; // hold PWM 
 
@@ -93,6 +100,9 @@ byte M2ArrayPower = 0; //Motor1 Array of Peltier
 
 #define COOL 0       // motor current direction for cooling effect 
 #define HEAT 1       // motor current direction for heating effect 
+    
+double tmp0; // thermocouple #1
+double tmp1; // thermocouple #2
 
 int tempDirection = HEAT; 
 
@@ -112,8 +122,6 @@ void setup(){
 
   pinMode(sysOnLED, OUTPUT); // LED system start
   
-  //pinMode(rangeGoLED, OUTPUT); // Range good to use
-  //pinMode(rangeStopLED, OUTPUT); // Outside range
   pinMode(targetLED, OUTPUT); // target gap LED 
   pinMode(PWMLED, OUTPUT); // hold PWM 
   
@@ -131,22 +139,14 @@ void setup(){
   //turn the PID on
   myPID_probe.SetMode(AUTOMATIC);   
   myPID_plate.SetMode(AUTOMATIC);
-
-  //Serial.print("#Target temp is ");
-  //Serial.println(Probe_targetTemp);
   
   Serial.println("Int-Temp,Ext-Temp,diff-Probe,diff-Plate,probeOffset,plateOffset,PWM,Video"); 
 }
 
 void loop(){
 
-  float currentTime = (millis() - startTime)/1000.0;
-
 // ##### Start Button trigger ##############################    
-  while (trigger == false){
-    //digitalWrite(rangeGoLED, LOW);
-    //digitalWrite(rangeStopLED, LOW);
-    
+  while (trigger == false){  
     if (trigger == false && digitalRead(buttonStart) == LOW && digitalRead(buttonStop) == HIGH){
       digitalWrite(sysOnLED, HIGH);
       trigger = true;
@@ -154,140 +154,141 @@ void loop(){
       digitalWrite(sysOnLED, LOW);
       trigger = false;
     }
- 
     delay (1000);
     startTime = millis();  
+    loopMillis = millis(); 
   }
 
-// ##### Video code ####################################
+// ##### Loop delay ####################################
+  if(millis() - loopMillis >= runInterval){ 
+    loopMillis = millis(); //store last time of loop
+    
+  // ##### Thermocouple code ##############################  
+    //delay(125); //delay is now happening as while loop //... can be as fast as ~100ms in continuous mode, 1 samp avg
+    read_thermocouple(tmp0, tmp1);
+  
+  // ################ PWM control ########################################
+    //PID 1.2.0
+    Setpoint_probe = caliProbe_targetTemp;
+    Input_probe = tmp0;
+    gap_probe = abs(Setpoint_probe-Input_probe); //distance away from setpoint
+  
+    Setpoint_plate = caliPlate_targetTemp;
+    Input_plate = tmp1;
+  
+    // ######### Target Light and probe K control ###############
+    if (gap_probe < 0.2 && (millis() - startTime) > (60000*3)){ // 3 min
+      digitalWrite(targetLED, HIGH);
+    } else {
+      digitalWrite(targetLED, LOW);
+    } 
+  
+    myPID_probe.SetTunings(Kp_probe, Ki_probe, Kd_probe);
+    myPID_probe.Compute();
+    
+    if(Output_probe > PWMmax) { //constrain scaling to not burn wire 
+      limitWireOutput = PWMmax; 
+    } else if (Output_probe < 0){
+      limitWireOutput = 0;
+    } else {
+      limitWireOutput = Output_probe;
+    }
+  
+    myPID_plate.Compute();
+    myPID_probe.SetTunings(Kp_plate, Ki_plate, Kd_plate);
+  
+    if(Output_plate > 255) { //constrain scaling to not burn wire 
+      limitPeltierOutput = 255; 
+    } else if (Output_plate < 0){
+      limitPeltierOutput = 0;
+    } else {
+      limitPeltierOutput = Output_plate;
+    }
+  
+    //set peltier direction for cooling or heating 
+    //need a better way to compare to room temperature 
+    if ((Plate_targetTemp - 20) > 0){
+      tempDirection = HEAT; 
+    } else if ((Plate_targetTemp - 20) < 0){
+      tempDirection = COOL; 
+    }
+    
+    if (endHeat == true){
+      limitWireOutput = 0;
+      limitPeltierOutput = 0;
+    }
+
+    digitalWrite(URC10_MOTOR_1_DIR, 1); //Board motor controler current direction for magnet wire
+    M1ArrayPower = (byte) limitWireOutput;  //set PWM byte from polynomial scaling 
+    analogWrite(URC10_MOTOR_1_PWM, M1ArrayPower);   //send PWM value to magnet wire
+      
+    digitalWrite(URC10_MOTOR_2_DIR, tempDirection);
+    M2ArrayPower = (byte) limitPeltierOutput;  //set PWM byte from polynomial scaling 
+    analogWrite(URC10_MOTOR_2_PWM, M2ArrayPower);   //send PWM value to Peltier   
+
+  // ################ Print variables ########################################
+  
+    diffProbeTarget = (caliProbe_targetTemp-tmp0)*10.0;
+    diffPlateTarget = (caliPlate_targetTemp-tmp1)*10.0;
+    
+    Serial.print(tmp0); // print temperature sensor probe 
+    Serial.print(",");
+ 
+    Serial.print(tmp1); // print temperature sensor plate
+    Serial.print(",");
+    
+    Serial.print(diffProbeTarget);
+    Serial.print(","); 
+    
+    Serial.print(diffPlateTarget);
+    Serial.print(","); 
+    Serial.print(probeOffset,1);
+    Serial.print(",");
+    Serial.print(plateOffset,1);
+    Serial.print(",");
+    Serial.print(M1ArrayPower);
+    Serial.print(",");
+    Serial.print(video_record);   
+
+    /* //Trouble Shooting
+    Serial.print(",");
+    Serial.print(M2ArrayPower);
+    Serial.print(",");
+    Serial.print((millis()-startTime)/1000); 
+    //Trouble Shooting 
+    */
+    Serial.println();
+  }
+// ##### End Loop delay ####################################
+
+// ##### Video sync ####################################
   if (digitalRead(videoPin) == LOW){
     video_record = 0 ;
   } else if (digitalRead(videoPin) == HIGH){
     video_record = 1 ;
   }  
 
-// ##### Thermocouple code ##############################  
-  delay(500);                                   // 500ms delay... can be as fast as ~100ms in continuous mode, 1 samp avg
-  
-  double tmp0; // thermocouple #1
-  double tmp1; // thermocouple #2
-  read_thermocouple(tmp0, tmp1);
+// ################ Assay controls ######################################## 
 
-// ################ PWM control ########################################
-
-  //PID 1.2.0
- 
-  Setpoint_probe = caliProbe_targetTemp;
-  Input_probe = tmp0;
-  gap_probe = abs(Setpoint_probe-Input_probe); //distance away from setpoint
-
-  Setpoint_plate = caliPlate_targetTemp;
-  Input_plate = tmp1;
-
-  // ######### Target Light and probe K control ###############
-  if (currentTime < (60*3)){ // 3 min
-    digitalWrite(targetLED, LOW);
-    Kp_probe = consKp_probe;
-    Ki_probe = consKi_probe;
-  } else if (gap_probe < 0.2 && currentTime > (60*3)){ // 3 min
-    digitalWrite(targetLED, HIGH);
-  } else {
-    digitalWrite(targetLED, LOW);
-    Kp_probe = aggKp_probe;
-    Ki_probe = aggKi_probe;
-  } 
-
-  myPID_probe.SetTunings(Kp_probe, Ki_probe, Kd_probe);
-  myPID_probe.Compute();
-  
-  if(Output_probe > PWMmax) { //constrain scaling to not burn wire 
-    limitWireOutput = PWMmax; 
-  } else if (Output_probe < 0){
-    limitWireOutput = 0;
-  } else {
-    limitWireOutput = Output_probe;
-  }
-
-  myPID_plate.Compute();
-  myPID_probe.SetTunings(Kp_plate, Ki_plate, Kd_plate);
-
-  if(Output_plate > 254) { //constrain scaling to not burn wire 
-    limitPeltierOutput = 254; 
-  } else if (Output_plate < 0){
-    limitPeltierOutput = 0;
-  } else {
-    limitPeltierOutput = Output_plate;
-  }
-
-  if (enterPWMhold == false && digitalRead(offsetUp) == LOW && digitalRead(offsetDown) == LOW){
+  if (digitalRead(offsetUp) == LOW && digitalRead(offsetDown) == HIGH && buttonPause == false){
+    probeOffset = probeOffset + 0.5;
+    caliProbe_targetTemp = Probe_targetTemp + probeOffset;
     digitalWrite(PWMLED, HIGH);
-    //holdingPWM = limitWireOutput; 
-    enterPWMhold = true;
-  } else if (enterPWMhold == true && digitalRead(offsetUp) == LOW && digitalRead(offsetDown) == LOW){
-    digitalWrite(PWMLED, LOW);
-    enterPWMhold = false;
-  }      
-
-  //set peltier direction for cooling or heating 
-  //need a better way to compare to room temperature 
-  if ((Plate_targetTemp - 20) > 0){
-    tempDirection = HEAT; 
-  } else if ((Plate_targetTemp - 20) < 0){
-    tempDirection = COOL; 
-  }
-  
-  if (endHeat == true){
-    limitWireOutput = 0;
-    limitPeltierOutput = 0;
+  } else if (digitalRead(offsetUp) == HIGH && digitalRead(offsetDown) == LOW && buttonPause == false){
+    probeOffset = probeOffset - 0.5;
+    caliProbe_targetTemp = Probe_targetTemp + probeOffset;
+    digitalWrite(PWMLED, HIGH);
   }
 
-  if (enterPWMhold == false){
-    M1ArrayPower = (byte) limitWireOutput;  //set PWM byte from polynomial scaling 
-  } else if (enterPWMhold == true){
-    //M1ArrayPower = (byte) holdingPWM;
-    M1ArrayPower = (byte) limitWireOutput;
+  if (digitalRead(offsetUp) == LOW || digitalRead(offsetDown) == LOW && buttonPause == false){
+    buttonTime = millis();
+    buttonPause = true; 
+  } else if (millis() - buttonTime > 100){
+    buttonPause = false; 
   }
 
-  digitalWrite(URC10_MOTOR_1_DIR, 1); //Board motor controler current direction for magnet wire
-  analogWrite(URC10_MOTOR_1_PWM, M1ArrayPower);   //send PWM value to magnet wire
-    
-  digitalWrite(URC10_MOTOR_2_DIR, tempDirection);
-  M2ArrayPower = (byte) limitPeltierOutput;  //set PWM byte from polynomial scaling 
-  analogWrite(URC10_MOTOR_2_PWM, M2ArrayPower);   //send PWM value to Peltier   
-
-// ################ Print variables control ########################################
-
-  float diffProbeTarget = (caliProbe_targetTemp-tmp0)*10;
-  float diffPlateTarget = (caliPlate_targetTemp-tmp1)*10;
-   
-  Serial.print(tmp0); // print temperature sensor 0 
-  Serial.print(",");
-  Serial.print(tmp1); // print temperature sensor 1
-  Serial.print(",");
-  Serial.print(diffProbeTarget);
-  Serial.print(","); 
-  Serial.print(diffPlateTarget);
-  Serial.print(","); 
-  Serial.print(probeOffset,1);
-  Serial.print(",");
-  Serial.print(plateOffset,1);
-  Serial.print(",");
-  Serial.print(M1ArrayPower);
-  Serial.print(",");
-  Serial.print(video_record);
-
-  //Trouble Shooting
-  /*
-  Serial.print(",");
-  Serial.print(currentTime);
-  Serial.print(",");
-  Serial.print(M2ArrayPower);
-  //Trouble Shooting 
-  */
-  
-  Serial.println();
-
-// ################ Assay controls ########################################
+  //caliPlate_targetTemp = Plate_targetTemp + plateOffset;
+  digitalWrite(PWMLED, LOW);
 
   if (digitalRead(buttonStart) == HIGH && digitalRead(buttonStop) == LOW){
     digitalWrite(sysOnLED, LOW);
@@ -295,34 +296,10 @@ void loop(){
     Serial.println();
   }
 
-  if (enterPWMhold == false){
-    if (digitalRead(offsetUp) == LOW && digitalRead(offsetDown) == HIGH){
-      probeOffset = probeOffset + 0.5;
-    }
-    if (digitalRead(offsetUp) == HIGH && digitalRead(offsetDown) == LOW){
-      probeOffset = probeOffset - 0.5;
-    }
-  } else if (enterPWMhold == true){
-    if (digitalRead(offsetUp) == LOW && digitalRead(offsetDown) == HIGH){
-      plateOffset = plateOffset + 0.5;
-    }
-    if (digitalRead(offsetUp) == HIGH && digitalRead(offsetDown) == LOW){
-      plateOffset = plateOffset - 0.5;
-    }
-
-    if (digitalRead(offsetUp) == LOW && digitalRead(offsetDown) == HIGH){
-      holdingPWM = holdingPWM + 1;
-    }
-    if (digitalRead(offsetUp) == HIGH && digitalRead(offsetDown) == LOW){
-      holdingPWM = holdingPWM - 1;
-    }
-  }
-
-  caliProbe_targetTemp = Probe_targetTemp + probeOffset;
-  caliPlate_targetTemp = Plate_targetTemp + plateOffset;
-
-  while (currentTime > assayTime){ //end program when assay length is done and hold in loop 
+ // ################ Auto Shutoff ########################################
+  while (millis() - startTime > assayTime){ //end program when assay length is done and hold in loop 
     analogWrite(URC10_MOTOR_1_PWM, 0); //turn wire off 
+    analogWrite(URC10_MOTOR_2_PWM, 0); //turn peltier off
     digitalWrite(sysOnLED, LOW);
     Serial.println();
     Serial.print("DONE,");
